49082c9c3cd1ecb75b6ad114c00dae59216c6ab3
ngshijun <shijunng2010@gmail.com>
2025-06-20 22:55:53 +0800

    update root path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
-import Dashboard from '@/views/DashBoard.vue'
+import DashBoard from '@/views/DashBoard.vue'
 import Inventory from '@/views/Inventory.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    redirect: '/dashboard'
-  },
-  {
-    path: '/dashboard',
     name: 'Dashboard',
-    component: Dashboard
+    component: DashBoard
   },
   {
     path: '/inventory',
